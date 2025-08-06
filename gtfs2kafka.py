9c3e5c9806abb846d4425e8815be64e0126fee0c
Will Barbour <12778879+barbourww@users.noreply.github.com>
2025-08-05 20:51:50 -0500

    Making this into a template for the actual code

--- gtfs2kafka.py
+++ gtfs2kafka.py
@@ -1,158 +1,135 @@
-import logging
-logger = logging.getLogger(__name__)
-import argparse
+import threading
 
+import requests
+import argparse
 import time
+from google.transit import gtfs_realtime_pb2
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import LineString, Point
+import zipfile
+import io
 import json
+import time
 import os
 
 import kafka_confluent as kc
 
+import logging
+logger = logging.getLogger(__name__)
 from dotenv import load_dotenv
 load_dotenv()
 
-class LaddmsRecorder():
-    def __init__(self, cfg, kc):
-        self.cfg = cfg
-        self.kc = kc
-        self.ousterObjectSubscriber = ouster.Ouster(host=cfg['OUSTER_HOST'], port=3302)
-        self.ousterObjectSubscriber.packet_callback = self.newOusterObjectPacket
-            
-#        self.ousterOccupationSubscriber = ouster.Ouster(host=cfg['OUSTER_HOST'], port=3303)
-#        self.ousterOccupationSubscriber.packet_callback = self.newOusterOccupationPacket
-        
-        self.objectPacketCount = 0
-        self.zonePacketCount = 0
-        
-        self.device_id = cfg['INTERSECTION_ID']
-        
+class GTFSStaticProducer():
+    def __init__(self, receiver_config, kafka_config):
+        self.config = receiver_config
+        self.kc = kc.KafkaConfluentHelper(kafka_config)
+
         self.topic_name = "unassinged"
         self.partition_key = "0"
         
 
-    def start(self):
-#        self.sql_connection = db_laddms.make_sql_connection(self.cfg)
-#        self.sql_cursor = db_laddms.make_sql_cursor(self.sql_connection)
-        self.update_id()
-        
-        self.ousterObjectSubscriber.start()
-#        self.ousterOccupationSubscriber.start()
-        
-        
-    def update_id(self):
-        # Client ID is always "detect-client"
-        client_id = "detect-client" #self.cfg['OUSTER_USER'] # HACK temp fix
-        client_secret = self.cfg['OUSTER_SECRET']
-        # Base URL is the IP address or domain name of the server hosting Gemini Detect
-        base_url = self.cfg['OUSTER_TCP_URL']
-        # Token URL should always be as shown below
-        token_url = f"{base_url}/auth/realms/detect/protocol/openid-connect/token"
-    
-        api = gemini.AuthenticatedAPI(client_id, client_secret, token_url, base_url)
-#        response = api.get("perception/api/v1/settings")
-        response = api.get("lidar-hub/api/v1/diagnostics")
-#        print( str(response))
-#        print(json.dumps(response, indent=3))
-#        print(response['hardware_id'])
-#        print(response['compute']['host_name'])
-#        print(response['compute']['mac_address'])
-#        self.partition_key = response['hardware_id'].replace("*", "")
-        self.partition_key = str(self.device_id)
-#        self.topic = 'postgres-data'
-        self.topic = 'my-topic'
-        
-        
-    def update_zone_info(self):
-        # Client ID is always "detect-client"
-        client_id = "detect-client"
-        # Client secret is retreived from the Keycloak Admin Console -> Clients(side bar) ->
-        # detect-client(clients list) -> Credentials (tab)
-        client_secret = self.cfg['OUSTER_SECRET']
-        # Base URL is the IP address or domain name of the server hosting Gemini Detect
-        #base_url = "https://192.168.2.3:4443"
-        base_url = self.cfg['OUSTER_TCP_URL']
-        # Token URL should always be as shown below
-        token_url = f"{base_url}/auth/realms/detect/protocol/openid-connect/token"
+    def pull_static_gtfs(self):
+        pass
 
-        api = gemini.AuthenticatedAPI(client_id, client_secret, token_url, base_url)
-        response = api.get("lidar-hub/api/v1/event_zones")
-#        print(json.dumps(response, indent=2))
-#        with open("event_zones.txt", "w") as text_file:
-#            text_file.write(json.dumps(response, indent=2))
-#        pass
-#        db_laddms.insert_event_zones(json_data=response, device_id=self.device_id, use_db_cursor=self.sql_cursor)
-        self.kc.send(topic=self.topic, key=self.partition_key, json_data=response, headers=[('device',b'ouster'), ('detection',b'zones')])
-        
+    def produce_static_route_trips_to_kafka(self):
+        pass
 
-    def newOusterObjectPacket(self, packet):
-#        self.objectPacketCount += 1
-#        if self.objectPacketCount >= 100:
-#            self.objectPacketCount = 0
-#            print("Got 100 object packets!")
-#        db_laddms.insert_object_detections(json_data=packet, intersection_id=self.cfg['INTERSECTION_ID'], device_id=self.device_id, use_db_cursor=self.sql_cursor)
-#        print("Objects inserted at " + str(time.time()))
-#        logger.info("New PAcket!")
-        self.kc.send(topic=self.topic, key=self.partition_key, json_data=packet, headers=[('device',b'ouster'), ('detection',b'objects')])
-        
-    def newOusterOccupationPacket(self, packet):
-#        self.zonePacketCount += 1
-#        if self.zonePacketCount >= 100:
-#            self.zonePacketCount = 0
-#            print("Got 100 zone packets!")
-#        db_laddms.insert_zone_occupation(json_data=packet, device_id=self.device_id, use_db_cursor=self.sql_cursor)
-#        print("Occupations inserted at " + str(time.time()))
-        self.kc.send(topic=self.topic, key=self.partition_key, json_data=packet, headers=[('device',b'ouster'), ('detection',b'occupations')])
-        
+    def produce_static_stops_to_kafka(self):
+        pass
+
+
+class GTFSRTVehicleProducer():
+    def __init__(self, receiver_config, kafka_config):
+        self.config = receiver_config
+        self.kc = kc.KafkaConfluentHelper(kafka_config)
+
+        self.topic_name = "unassinged"
+        self.partition_key = "0"
+
+    def receive_vehicle_positions(self):
+        # self.kc.send(topic=self.topic, key=self.partition_key, json_data=response, headers=[('device',b'ouster'), ('detection',b'zones')])
+        pass
+
+
+class GTFSRTTransitUpdatesProducer():
+    def __init__(self, receiver_config, kafka_config):
+        self.config = receiver_config
+        self.kc = kc.KafkaConfluentHelper(kafka_config)
+
+        self.topic_name = "unassinged"
+        self.partition_key = "0"
+
+    def receive_transit_updates(self, packet):
+        # self.kc.send(topic=self.topic, key=self.partition_key, json_data=packet, headers=[('device',b'ouster'), ('detection',b'objects')])
+        pass
+
+
+class GTFSRTAlertsProducer():
+    def __init__(self, receiver_config, kafka_config):
+        self.config = receiver_config
+        self.kc = kc.KafkaConfluentHelper(kafka_config)
+
+        self.topic_name = "unassinged"
+        self.partition_key = "0"
+
+    def receive_service_alerts(self, packet):
+        # self.kc.send(topic=self.topic, key=self.partition_key, json_data=packet, headers=[('device',b'ouster'), ('detection',b'occupations')])
+        pass
+
+
+def update_gtfs_static():
+    pass
+
+def update_gtfs_rt_alerts():
+    pass
+
+def update_gtfs_rt_transit_updates():
+    pass
+
+def update_gtfs_rt_vehicles():
+    pass
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Ouster to Kafka')
-    parser.add_argument('-i', '--intersection_id', default=None)
-    parser.add_argument('-o', '--host', default=None)
-    parser.add_argument('-s', '--secret', default=None)
-    parser.add_argument('-d', '--device', default=None)
-    
-    parser.add_argument('-k', '--kafkahost', default=None)
-    parser.add_argument('-u', '--kafkauser', default=None)
-    parser.add_argument('-p', '--kafkapassword', default=None)
-    args = parser.parse_args()
-    
-    if not args.host and not os.environ.get('OUSTER_HOST'):
-        print("Error, OUSTER_HOST not defined!")
-        quit()
-    url = ("https://" + str(args.host) + ":443") if args.host else ("https://" + os.environ.get('OUSTER_HOST') + ":443")
-    config = {
-        'OUSTER_HOST': args.host if args.host else os.environ.get('OUSTER_HOST'),
-        'OUSTER_TCP_URL': url,
-        'OUSTER_SECRET': args.secret if args.secret else os.environ.get('OUSTER_SECRET'),
-        'DEVICE_ID': args.device if args.device else os.environ.get('DEVICE_ID'),
-        'INTERSECTION_ID': args.intersection_id if args.intersection_id else int(os.environ.get('INTERSECTION_ID')),
+    static_config = {
+        'GTFS_STATIC_URL': os.environ.get('GTFS_STATIC_URL'),
+        'GTFS_STATIC_UPDATE_MINS': os.environ.get('GTFS_STATIC_UPDATE_MINS'),
+    }
+
+    rt_config = {
+        'GTFS_RT_ALERTS_URL': os.environ.get('GTFS_RT_ALERTS_URL'),
+        'GTFS_RT_ALERTS_UPDATE_SECS': os.environ.get('GTFS_RT_ALERTS_UPDATE_SECS'),
+
+        'GTFS_RT_TRIPS_URL': os.environ.get('GTFS_RT_TRIPS_URL'),
+        'GTFS_RT_TRIPS_UPDATE_SECS': os.environ.get('GTFS_RT_TRIPS_UPDATE_SECS'),
+
+        'GTFS_RT_VEHICLES_URL': os.environ.get('GTFS_RT_VEHICLES_URL'),
+        'GTFS_RT_VEHICLES_UPDATE_SECS': os.environ.get('GTFS_RT_VEHICLES_UPDATE_SECS'),
     }
     
-#    print(config)
-#    quit()
-    
-    config_kafka = {
-        'KAFKA_BOOTSTRAP': args.kafkahost if args.kafkahost else os.environ.get('KAFKA_BOOTSTRAP'),
-        'KAFKA_USER':  args.kafkauser if args.kafkauser else os.environ.get('KAFKA_USER'),
-        'KAFKA_PASSWORD': args.kafkapassword if args.kafkapassword else os.environ.get('KAFKA_PASSWORD'),
+    common_kafka_config = {
+        'KAFKA_BOOTSTRAP': os.environ.get('KAFKA_BOOTSTRAP'),
+        'KAFKA_USER':  os.environ.get('KAFKA_USER'),
+        'KAFKA_PASSWORD': os.environ.get('KAFKA_PASSWORD'),
     }
-    print(config_kafka)
     
     log_path = str(os.environ.get('LOG_PATH')) if os.environ.get('LOG_PATH') else "."
-    
-    kafkaConfluent = kc.KafkaConfluentHelper(config_kafka)
-    
-    loggerFile = log_path + '/ouster2kafka-' + str(config['INTERSECTION_ID']) + '.log'
+    loggerFile = log_path + '/gtfs2kafka.log'
     print('Saving logs to: ' + loggerFile)
     FORMAT = '%(asctime)s %(message)s'
     logging.basicConfig(filename=loggerFile, level=logging.INFO, format=FORMAT)
 
-    logger.info("Running... for ouster at " + config['OUSTER_HOST'])
-    logger.info("Intersection Id: " + str(config['INTERSECTION_ID']))
+    logger.info("Starting GTFS to Kafka producer.")
 
-    myLaddmsRecorder = LaddmsRecorder(config, kafkaConfluent)
-    myLaddmsRecorder.start()
+    static_receiver = GTFSStaticProducer(static_config, kafka_config=common_kafka_config)
+    rt_alerts_receiver = GTFSRTAlertsProducer(rt_config, kafka_config=common_kafka_config)
+    rt_transit_receiver = GTFSRTTransitUpdatesProducer(rt_config, kafka_config=common_kafka_config)
+    rt_vehicles_receiver = GTFSRTVehicleProducer(rt_config, kafka_config=common_kafka_config)
+
         
     while True:
-        myLaddmsRecorder.update_zone_info()
-        time.sleep(60)
+        threading.Thread(target=update_gtfs_static)
+        threading.Thread(target=update_gtfs_rt_alerts)
+        threading.Thread(target=update_gtfs_rt_vehicles)
+        threading.Thread(target=update_gtfs_rt_transit_updates)
         
--- kafka_confluent.py
+++ kafka_confluent.py
@@ -1,21 +1,21 @@
 import asyncio
 import json
-
-
-#from kafka import KafkaProducer
-#from kafka.admin import KafkaAdminClient, NewTopic
+import os
 
 from confluent_kafka import Producer
+
+import logging
+logger = logging.getLogger(__name__)
+from dotenv import load_dotenv
+load_dotenv()
 
 
 class KafkaConfluentHelper():
     def __init__(self, config):
-#        self.conf = config
     
         self.conf = {
             'bootstrap.servers': config['KAFKA_BOOTSTRAP'],
             'security.protocol': 'SASL_SSL',
-        #    'ssl.ca.location': 'kafka-cluster-ca.crt',
             'ssl.ca.location': 'strimzi-ca.crt',
             'sasl.mechanism': 'SCRAM-SHA-512',
             'sasl.username': config['KAFKA_USER'],
@@ -33,54 +33,35 @@ class KafkaConfluentHelper():
             print(f"- id: {broker.id}, host: {broker.host}, port: {broker.port}")
 
     def on_send_success(self, record_metadata):
-        #print(record_metadata.topic)
-        #print(record_metadata.partition)
-        #print(record_metadata.offset)
         pass
 
     def on_send_error(self, excp):
-        log.error('I am an errback', exc_info=excp)
+        logger.error('I am an errback', exc_info=excp)
         
     def delivery_report(self, err, msg):
         if err is not None:
             print(f'Delivery failed: {err}')
-#        else:
-#            print(f'Message delivered to {msg.topic()} [{msg.partition()}]')
-            
-            
+
     def send(self, topic, key,  json_data, partition=None, headers=None):
-    #    producer.send(topic, key=key.encode('utf-8'), value=json.dumps(json_data).encode('utf-8'),
-    #    partition=partition, headers=headers).add_callback(on_send_success).add_errback(on_send_error)
-        
-        self.producer.produce(topic, key=key.encode('utf-8'), value=json.dumps(json_data).encode('utf-8'), headers=headers, callback=self.delivery_report)
+        self.producer.produce(topic,
+                              key=key.encode('utf-8'),
+                              value=json.dumps(json_data).encode('utf-8'),
+                              headers=headers,
+                              callback=self.delivery_report)
         self.producer.poll(0)
-        
-    #    producer = EventHubProducerClient.from_connection_string(
-    #        conn_str=EVENT_HUB_CONNECTION_STR, eventhub_name=EVENT_HUB_NAME
-    #    )
-    #
-    #    async with producer:
-    #        # Create a batch.
-    ##        event_data_batch = await producer.create_batch()
-    #
-    #        # Add events to the batch.
-    #        print(json.dumps(packaged_data))
-    ##        event_data_batch.add(EventData(json.dumps(packaged_data)))
-    #
-    #        # Send the batch of events to the event hub.
-    ##        await producer.send_batch(event_data_batch)
-    #
-    #        await producer.send_event(event_data=EventData(json.dumps(packaged_data)), partition_key=packaged_data['segment_id'])
+
 
 if __name__ == "__main__":
+    common_kafka_config = {
+        'KAFKA_BOOTSTRAP': os.environ.get('KAFKA_BOOTSTRAP'),
+        'KAFKA_USER': os.environ.get('KAFKA_USER'),
+        'KAFKA_PASSWORD': os.environ.get('KAFKA_PASSWORD'),
+    }
+    kc = KafkaConfluentHelper(common_kafka_config)
+
     new_data = {
         'test': "hello"
     }
-    packaged_data = package(new_data)
-#    print(packaged_data)
-    
-#    asyncio.run(send(packaged_data))
     headers = [('key1', b'value1'), ('key2', b'value2')]
-    send(topic='postgres-data', key='my_key', partition=0, json_data=packaged_data, headers=headers)
-    producer.flush()
+    kc.send(topic='postgres-data', key='my_key', partition=0, json_data=new_data, headers=headers)
     
